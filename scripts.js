a1ab50d61d46136cb2c6368e960ec96bba4c0681
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-08-27 15:01:57 -0300

    tempo ajustado para 5 10 e 30

--- scripts.js
+++ scripts.js
@@ -105,5 +105,11 @@ document.getElementById('addLinkForm').addEventListener('submit', addLink);
 // Executa a função fetchSitesStatus imediatamente ao carregar a página
 fetchSitesStatus();
 
-// Verifica o status dos sites a cada 5 minutos
-setInterval(fetchSitesStatus, 5 * 60 * 1000);
+// Opções de intervalo: 5 minutos, 10 minutos ou 60 minutos
+const intervalOption = 10; // Altere este valor para 5, 10 ou 60
+
+// Define o intervalo com base na opção selecionada (em milissegundos)
+const intervalMillis = intervalOption * 60 * 1000;
+
+// Verifica o status dos sites com o intervalo selecionado
+setInterval(fetchSitesStatus, intervalMillis);
